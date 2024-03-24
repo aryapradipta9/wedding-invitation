50be81a4f9875b3a2900d01f040aed59260e8ff1
aryapradipta9 <aryapradipta9@gmail.com>
2024-03-24 18:45:12 +0800

    add gallery

--- src/app/[id]/gallery.tsx
+++ src/app/[id]/gallery.tsx
@@ -1,4 +1,14 @@
 import PhotoAlbum from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+// import optional lightbox plugins
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import { useState } from "react";
 
 const photos = [
   { src: "/gallery/TB2_2142.jpg", width: 800, height: 600 },
@@ -12,5 +22,23 @@ const photos = [
 ];
 
 export default function Gallery() {
-  return <PhotoAlbum layout="rows" photos={photos} />;
+  const [index, setIndex] = useState(-1);
+
+  return (
+    <>
+      <PhotoAlbum
+        layout="rows"
+        photos={photos}
+        onClick={({ index }) => setIndex(index)}
+      />
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
+    </>
+  );
 }
